6ab15e77cf1262b1026b59aec3f4304f7aeaeffb
andreajiao <andreajiao@gmail.com>
2023-08-10 18:20:20 +0200

    build expenseform-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import ExpenseList from './expense-tracker/components/ExpenseList';
 import { useState } from 'react';
 import ExpenseFilter from './expense-tracker/components/ExpenseFilter';
+import ExpenseForm from './expense-tracker/components/ExpenseForm';
+
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const categories = ["Groceries", "Entertainment", "Utilities"]
 
 function App(){
   const [selectedCategory, setSelectedCategory] =useState("")
@@ -20,6 +25,9 @@ function App(){
 
   return (
     <div>
+      <div className="mb-5">
+        <ExpenseForm/>
+      </div>
       <div className="mb-3">
          <ExpenseFilter onSelectCategory={category => setSelectedCategory(category)}/>
       </div>
