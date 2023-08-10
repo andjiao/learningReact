2d8edc71538032ef9e263011a63af288ec589e9d
andreajiao <andreajiao@gmail.com>
2023-08-10 17:39:30 +0200

    build the ExpenseList-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import Form from "./components/Form";
+import ExpenseList from './expense-tracker/components/ExpenseList';
+import { useState } from 'react';
 
 function App(){
+  const [expenses, setExpenses] = useState([
+    {id:1, description:"aaa", amount:10, category: "utilities"},
+    {id:2, description:"bbb", amount:10, category: "utilities"},
+    {id:3, description:"ccc", amount:10, category: "utilities"},
+    {id:4, description:"ddd", amount:10, category: "utilities"},
+  ])
+
 
 
 
   return (
     <div>
-     <Form/>     
+     <ExpenseList expenses={expenses} onDelete={(id)=> setExpenses(expenses.filter(e => e.id!==id ))}/>  {/* getting all the expenses except the one with the given id */}
     </div>
  
   )
