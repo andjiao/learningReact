b4c917c452771301465137a88faad00ea73d7453
andreajiao <andreajiao@gmail.com>
2023-08-10 11:31:47 +0200

    formvalidation with Zod

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,18 @@
 
 import { useForm, FieldValues } from 'react-hook-form'
+import {z} from "zod"
+import {zodResolver} from "@hookform/resolvers/zod"
 
-interface FormData {
-    name:string;
-    age:number;
-}
+const schema = z.object({
+    name:z.string().min(3, {message:"name must be at least 3 charachters"}),
+    age: z.number( {invalid_type_error:"Agefield is required"}).min(18, {message:"age must be at least 18"})
+})
+/* on line14: this returns a typescript type, which is similar to an interface. so using type we can deinfe a shape.
+
+We need to instal hookform/resolvers library, which include resolvers 
+for various schema-based validation-librarys like zod, yup, joi etc.
+*/
+type FormData = z.infer<typeof schema> 
 
 const Form = () => {
     
@@ -16,7 +24,7 @@ const Form = () => {
         on line 19 we call the useForm-hook and pass the interface, 
         this securee automcomplete when writting errors on line 33 and 34
         */
-    } =useForm<FormData>() 
+    } =useForm<FormData>({resolver: zodResolver(schema)}) 
 
     const onSubmit = (data: FieldValues) => console.log(data)
     
@@ -26,20 +34,21 @@ const Form = () => {
           <div className="mb-3">
           <label className="form-label" htmlFor='name' >Name</label>
           <input 
-          {...register ("name", {required:true, minLength:3})} 
+          {...register ("name", )} 
           id="name" 
           type="text" 
           className="form-control" />
-         {errors.name?.type === "required" && <p className='text-danger'>The name field is required</p>}
-        {errors.name?.type==="minLenght" && <p className='text-danger'>The name must be at least three characters</p>}
+         {errors.name && <p className='text-danger'>{errors.name.message}</p>}
+       
       </div>
 
       <div className="mb-3"><label htmlFor="age" className="form-label">Age</label>
       <input 
-      {...register("age")}
+      {...register("age", {valueAsNumber:true})} // saying to react this should be interpret as a number
          id="age" 
           type="number" 
           className="form-control" />
+          {errors.age && <p className='text-danger'>{errors.age.message}</p>}
      
       </div>
       <button className="btn btn-primary" type="submit">Submit</button>
