2b651c9d34a1e7a6800acc79de8353acb15b779f
andreajiao <andreajiao@gmail.com>
2023-08-11 09:18:25 +0200

    finished expense-tracker

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import ExpenseList from './expense-tracker/components/ExpenseList';
 import { useState } from 'react';
 import ExpenseFilter from './expense-tracker/components/ExpenseFilter';
 import ExpenseForm from './expense-tracker/components/ExpenseForm';
+// import categories from './expense-tracker/categories';
 
-
-// eslint-disable-next-line react-refresh/only-export-components
-export const categories = ["Groceries", "Entertainment", "Utilities"]
 
 function App(){
   const [selectedCategory, setSelectedCategory] =useState("")
@@ -26,7 +24,7 @@ function App(){
   return (
     <div>
       <div className="mb-5">
-        <ExpenseForm/>
+        <ExpenseForm onSubmit={expense => setExpenses([...expenses, {...expense, id:expenses.length+1}])}/>
       </div>
       <div className="mb-3">
          <ExpenseFilter onSelectCategory={category => setSelectedCategory(category)}/>
---
+++ src/expense-tracker/categories.ts
@@ -0,0 +1,3 @@
+const categories = ["Groceries", "Entertainment", "Utilities"] as const; // as cons makes the array a read-only
+
+export default categories;
