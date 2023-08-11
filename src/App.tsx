6cb2ef86f5c69020c5102de1cbb7db73071b78e7
andreajiao <andreajiao@gmail.com>
2023-08-11 10:05:06 +0200

    how to use useEEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,20 @@
-import ExpenseList from './expense-tracker/components/ExpenseList';
+import ProductList from "./components/ProductList";
 import { useState } from 'react';
-import ExpenseFilter from './expense-tracker/components/ExpenseFilter';
-import ExpenseForm from './expense-tracker/components/ExpenseForm';
-// import categories from './expense-tracker/categories';
-
-
 function App(){
-  const [selectedCategory, setSelectedCategory] =useState("")
-  const [expenses, setExpenses] = useState([
-    {id:1, description:"aaa", amount:10, category: "Utilities"},
-    {id:2, description:"bbb", amount:10, category: "Groceries"},
-    {id:3, description:"ccc", amount:10, category: "Utilities"},
-    {id:4, description:"ddd", amount:10, category: "Utilities"},
-  ])
-
-  const visibleExpenses = selectedCategory
-  ? expenses.filter(e => e.category === selectedCategory)
-  : expenses;
-
-
-
-
+  const [category,setCategory]=useState('')
+ 
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm onSubmit={expense => setExpenses([...expenses, {...expense, id:expenses.length+1}])}/>
-      </div>
-      <div className="mb-3">
-         <ExpenseFilter onSelectCategory={category => setSelectedCategory(category)}/>
-      </div>
-    
-     <ExpenseList 
-     expenses={visibleExpenses} 
-     onDelete={(id)=> setExpenses(expenses.filter(e => e.id!==id ))}/>  {/* getting all the expenses except the one with the given id */}
+      <select className="form-select" onChange ={(event)=>setCategory(event.target.value)}>
+         <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">HouseHold</option>
+       
+        
+      </select>
+      <ProductList category={category}/>
     </div>
+       
  
   )
 }
