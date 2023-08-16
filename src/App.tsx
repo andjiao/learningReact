4a91e14e7b2fd61b4c8729118042767bf16dfa52
andreajiao <andreajiao@gmail.com>
2023-08-16 08:39:43 +0200

    working with async and await

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import axios from "axios"
+import axios, { AxiosError } from "axios"
 import { useState, useEffect } from 'react';
 
 interface User{
@@ -12,9 +12,17 @@ function App(){
   const  [error, setError] = useState("")
 
   useEffect(()=>{
-    axios.get <User[]>('https://jsonplaceholderx.typicode.com/users')
-    .then(res => setUsers(res.data))
-    .catch(err => setError(err.message))
+    const fetchUsers = async () =>{
+      try {
+         const res = await axios
+      .get <User[]>('https://jsonplaceholderx.typicode.com/users')
+      setUsers(res.data)
+      } catch(err){
+        setError((err as AxiosError).message)
+      }
+     
+    }
+    fetchUsers()
   }, [])
 
  
